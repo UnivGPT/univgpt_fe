5276591d148fda22f49a60615fafbb729e837652
jiwoo <han_jiwoo@snu.ac.kr>
2023-07-16 21:35:20 +0900

    [feat] mypage1

--- src/routes/MyPage.jsx
+++ src/routes/MyPage.jsx
@@ -5,13 +5,15 @@ import { Link } from "react-router-dom";
 const MyPage = (user) => {
   return (
     <div>
-      <div>
-        <HiUserCircle size="150" />
-        <div>
-          <div>{user.name} 님 환영합니다!</div>
-          <Link to="">나의 프롬프트 {/*myprompt.count*/}개</Link>
-          <Link to="">스크랩한 프롬프트 {/*prompt.count*/}개</Link>
-        </div>
+      <div className="flex flex-row bg-white rounded-3xl m-5 p-5 text-black">
+				<HiUserCircle size="150" />
+				<div className="flex flex-col">	
+					<div>
+						<div>{user.name} 님 환영합니다!</div>
+						<Link to="" className="button">나의 프롬프트 {/*myprompt.count*/}개</Link>
+						<Link to="" className="button">스크랩한 프롬프트 {/*prompt.count*/}개</Link>
+					</div>
+				</div>
       </div>
     </div>
   );
