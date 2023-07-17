e0c06fb1b6ab12b320865d9913a0a2d9b4fa3942
Kim Nam Hee <103100783+kimnamheeee@users.noreply.github.com>
2023-07-18 00:36:56 +0900

    feat: partially initialize naver login

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import PromptDetailPage from "./routes/PromptDetailPage";
 import PromptEditPage from "./routes/PromptEditPage";
 import PromptMakePage from "./routes/PromptMakePage";
 import InfoCheck from "./routes/InfoCheck";
+import NaverCallBack from "./routes/NaverCallBack";
 
 import "./App.css";
 
@@ -18,16 +19,17 @@ function App() {
       <BrowserRouter>
         <Header />
         <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/infoedit" element={<InfoEditPage />} />
-        <Route path="/mypage" element={<MyPage />} />
-        <Route path="/signin" element={<SignInPage />} />
-        <Route path="/signup" element={<SignUpPage />} />
-        <Route path="/:promptId" element={<PromptDetailPage />} />
-        <Route path="/:promptId/edit" element={<PromptEditPage />} />
-        <Route path="/:promptId/make" element={<PromptMakePage/>} />
-        <Route path="/infocheck" element={<InfoCheck />} />
-        </Routes>  
+          <Route path="/" element={<HomePage />} />
+          <Route path="/infoedit" element={<InfoEditPage />} />
+          <Route path="/mypage" element={<MyPage />} />
+          <Route path="/signin" element={<SignInPage />} />
+          <Route path="/signup" element={<SignUpPage />} />
+          <Route path="/:promptId" element={<PromptDetailPage />} />
+          <Route path="/:promptId/edit" element={<PromptEditPage />} />
+          <Route path="/:promptId/make" element={<PromptMakePage />} />
+          <Route path="/infocheck" element={<InfoCheck />} />
+          <Route path="api/auth/callback/naver" element={<NaverCallBack />} />
+        </Routes>
       </BrowserRouter>
       {/*InfoPassword랑 InfoEdit는 나중에 url userId별로 바꿔야 함! 지금은 그냥 디자인 들어가서 보려고 이렇게 해놨어유*/}
     </div>
---
+++ src/routes/NaverCallBack.jsx
@@ -0,0 +1,41 @@
+import { useEffect } from "react";
+import axios from "axios";
+
+const NaverCallBack = () => {
+  useEffect(() => {
+    const params = new URL(document.location.toString()).searchParams;
+    const code = params.get("code");
+    const state = params.get("state");
+
+    console.log("code", code);
+    console.log("state", state);
+
+    const url = "http://127.0.0.1:8000/api/account/socials";
+    const data = {
+      code: code,
+      state: state,
+    };
+
+    axios
+      .get(url, { params: data })
+      .then((response) => {
+        console.log("Response", response.data);
+      })
+      .catch((error) => {
+        console.log("error", error);
+      });
+  });
+};
+
+// const REST_API_KEY = `${process.env.REACT_APP_NAVER_CLIENT_ID}`;
+// const REST_API_SECRET = `${process.env.REACT_APP_NAVER_CLIENT_SECRET}`;
+
+// 여기서 이렇게 하면 cors 에러 나니까 code랑 state를 장고 쪽으로 보내주고 나머지 처리는 장고에서 해야 할 듯...?
+// axios.get(
+//   `https://nid.naver.com/oauth2.0/token?client_id=${REST_API_KEY}&client_secret=${REST_API_SECRET}&grant_type=authorization_code&state=${state}&code=${code}`
+// );
+
+// 장고로 보내서 토큰 받고 유저 프로필 정보 받기 - 프론트로 전송 -> 보내는 건 code, state 받는 건 프로필정보(response), 토큰(cookie)
+// 프론트는 정보를 받으면 로그인이 완료된 거니까 홈 화면으로 리다이렉트
+
+export default NaverCallBack;
