bb8e18c2e7aaf8f2b078c3a4044a708e7a5d7270
JoeYoojin <uzinfamily@naver.com>
2023-07-19 20:59:45 +0900

    prompts.js edit

--- src/data/prompts.js
+++ src/data/prompts.js
@@ -19,7 +19,7 @@ const prompts = [
   },
   {
     id: 2,
-    title: "여행 계획",
+    title: "여행 계획 짜기",
     description:
       "여행 장소와 숙박 일정 등을 입력하면 여행 계획을 짜주는 프롬프트(여행용)",
     content: "#장소에 #숙박일정으로 놀러갈거야, 여행 계획 짜줘",
@@ -37,10 +37,10 @@ const prompts = [
   },
   {
     id: 3,
-    title: "코드에 주석달기",
+    title: "북한학개론 PPT 요약하기",
     description:
-      "내가 작성한 코드를 한 줄씩 자세하게 설명해주는 주석을 달아주는 프롬프트(과제용)",
-    content: "#코드에 주석 달아줘",
+      "북한학개론 기말고사 준비를 위한 요약노트 만들기(시험공부용)",
+    content: "#텍스트 내용을 500자로 요약해줘",
     author: { id: 1, username: "yoojin" },
     like_user: [1, 2],
     category: [
@@ -53,30 +53,28 @@ const prompts = [
     like: 73,
     view: 113,
   },
-  {
-    id: 4,
-    title: "코드에 주석달기",
-    description:
-      "내가 작성한 코드를 한 줄씩 자세하게 설명해주는 주석을 달아주는 프롬프트(과제용)",
-    content: "#코드에 주석 달아줘",
-    author: { id: 1, username: "yoojin" },
-    like_user: [1, 2],
-    category: [
-      {
-        id: 1,
-        name: "과제",
-      },
-    ],
-    created_at: "2023-07-14T04:19:50.658501Z",
-    like: 67,
-    view: 196,
-  },
+  {   id: 4,
+      title: "여행할 때 듣기 좋은 음악 추천받기",
+      description: "여행할 때 듣기 좋은 음악을 추천해주는 프롬프트(음악)",
+      content: "여행할 때 듣기 좋은 음악 추천해줘",
+      author: { id: 1, username: "yoojin" },
+      like_user: [1, 2],
+      category: [
+        {
+          id: 1,
+          name: "음악",
+        },
+      ],
+      created_at: "2023-07-14T04:19:50.658501Z",
+      like: 34,
+      view: 11,
+    },
   {
     id: 5,
-    title: "코드에 주석달기",
+    title: "발표 대본 만들기",
     description:
-      "내가 작성한 코드를 한 줄씩 자세하게 설명해주는 주석을 달아주는 프롬프트(과제용)",
-    content: "#코드에 주석 달아줘",
+      "자료조사 내용을 발표 대본 형태로 변환해주는 프롬프트(과제용)",
+    content: "#공적인 말투로 #3분 분량의 #발표 대본을 만들어줘",
     author: { id: 1, username: "yoojin" },
     like_user: [1, 2],
     category: [
@@ -107,22 +105,24 @@ const prompts = [
     like: 113,
     view: 33,
   },
+ 
   {
     id: 7,
-    title: "여행할 때 듣기 좋은 음악 추천",
-    description: "여행할 때 듣기 좋은 음악을 추천해주는 프롬프트(음악)",
-    content: "여행할 때 듣기 좋은 음악 추천해줘",
+    title: "코드에 주석달기",
+    description:
+      "내가 작성한 코드를 한 줄씩 자세하게 설명해주는 주석을 달아주는 프롬프트(과제용)",
+    content: "#코드에 주석 달아줘",
     author: { id: 1, username: "yoojin" },
     like_user: [1, 2],
     category: [
       {
         id: 1,
-        name: "음악",
+        name: "과제",
       },
     ],
     created_at: "2023-07-14T04:19:50.658501Z",
-    like: 34,
-    view: 11,
+    like: 113,
+    view: 33,
   },
 ];
 
