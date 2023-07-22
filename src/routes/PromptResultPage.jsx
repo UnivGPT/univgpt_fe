f88dcf186780917999d74a51d8ef3ce42793350a
JoeYoojin <uzinfamily@naver.com>
2023-07-22 14:31:34 +0900

    임시저장

--- src/routes/PromptResultPage.jsx
+++ src/routes/PromptResultPage.jsx
@@ -29,12 +29,13 @@ const PromptResultPage = () => {
         comment={comments[0]} />
     </div>
 
-    <form className="w-auto h-fit flex flex-col items-center bg-gray-200 text-black p-11 rounded-3xl ">
+    <div className="w-screen h-screen">
+    <form className="w-auto h-3/4 flex flex-col items-center bg-gray-200 text-black p-11 rounded-3xl ">
       {/*사용자 측 프롬프트 질문*/}
   <div className="flex justify-between mx-20">
-  <div className="bubble-a font-medium overflow-x-hidden overflow-y-scroll">각 지방의 내일 날씨입니다. 내일은 맑은 뒤 구름이 많이 끼겠고, 제주도와 울릉도 독도에는 한두 차례 눈이 오겠습니다. 아침 최저기온은 중서부 지방이 영하 23도에서 영하 14도, 영동과 남부지방은 영하 13도에서 영하 8도로 몹시 추운 날씨가 내일 아침까지도 계속되겠습니다.
+  <div className="bubble-a font-medium overflow-x-hidden overflow-y-auto">각 지방의 내일 날씨입니다. 내일은 맑은 뒤 구름이 많이 끼겠고, 제주도와 울릉도 독도에는 한두 차례 눈이 오겠습니다. 아침 최저기온은 중서부 지방이 영하 23도에서 영하 14도, 영동과 남부지방은 영하 13도에서 영하 8도로 몹시 추운 날씨가 내일 아침까지도 계속되겠습니다.
 그렇지만 낮 최고 기온은 영하 6도에서 영상 1도의 분포로 오늘보다 3-4도 가량 올라갈 전망입니다.
-날씨를 전해 드렸습니다.</div>
+</div>
   <div className="bubble-a:after"/>
   <HiUserCircle size="150" className="flex flex-col items-center ml-10"/>
   </div>
@@ -52,7 +53,7 @@ const PromptResultPage = () => {
   나가기
   </button>
   </form>
-  </div>
+  </div></div>
   );
 };
 
