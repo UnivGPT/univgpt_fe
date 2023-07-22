0e19a9b08b634c8e720d75b5115b5d515ac0b0e3
JoeYoojin <uzinfamily@naver.com>
2023-07-22 23:35:31 +0900

    fix: promptMakePage

--- src/routes/PromptMakePage.jsx
+++ src/routes/PromptMakePage.jsx
@@ -2,35 +2,32 @@ import { useState } from "react";
 import prompts from "../data/prompts";
 import users from "../data/users";
 import { useEffect } from "react";
+import trashBin from "../assets/images/trashbin.png";
 
 const PromptMakePage = () => {
-  {
-    /*+버튼 누르면 폼 생성*/
-  }
-
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
   const [form, setForm] = useState([]);
-  {
-    /*나중에 prompt에 넣어야*/
-  }
+  const [activatedChoices, setActivatedChoices] = useState([]);
+  const [category, setCategory] = useState("과제");
   const [content, setContent] = useState("");
-  //console.log(title);
-  //console.log(description);
-  //console.log(content);
   const [prompt, setPrompt] = useState({
     title: "",
     description: "",
     content: "",
   });
   useEffect(() => {
-    setPrompt({ title: title, description: description, content: content });
-  }, [title, description, content]);
-  //console.log(prompt);
+    setPrompt({
+      title: title,
+      description: description,
+      form: form,
+      content: content,
+      category: category,
+    });
+  }, [title, description, content, form, category]);
 
   return (
     <div className="w-screen h-screen flex justify-evenly">
-    
       {/*왼쪽 절반*/}
       <div className="bg-gpt-indigo w-1/2 h-full px-20 pb-6 pt-12">
         <h1 className="text-3xl font-bold mb-2.5">제목</h1>
@@ -62,20 +59,159 @@ const PromptMakePage = () => {
         <h1 className="text-2xl font-bold mb-2.5 flex flex-col items-center">
           ____________________________________________________________________
         </h1>
-        {/*input입력기*/}
+        {/*{#입력값} 생성기*/}
         <h1 className="text-3xl font-bold mt-6 mb-2.5">입력값</h1>
         <div className="h-2/5 flex flex-col items-center overflow-y-auto">
-          {form.map((el) => el)}{" "}
+          {form.map((el, idx) => (
+            <>
+              <div className="w-full flex flex-row justify-between">
+                <input
+                  key={"label" + idx}
+                  id={"label" + idx}
+                  className="input-b mb-2"
+                  onChange={(e) => {
+                    const editedForm = [...form][idx];
+                    editedForm["label"] = e.target.value;
+                    const newForm = [...form];
+                    newForm.splice(idx, 1, editedForm);
+                    setForm(newForm);
+                  }}
+                  placeholder="입력값 명칭"
+                  value={el.label}
+                ></input>
+                <select
+                  className="text-black w-28 h-13 ml-2 mb-2 rounded-xl font-medium text-l text-center shadow"
+                  key={"type" + idx}
+                  id={"type" + idx}
+                  onChange={(e) => {
+                    const editedForm = [...form][idx];
+                    editedForm["type"] = e.target.value;
+                    const newForm = [...form];
+                    newForm.splice(idx, 1, editedForm);
+                    setForm(newForm);
+                    if (e.target.value === "객관식") {
+                      setActivatedChoices([...activatedChoices, idx]);
+                    } else {
+                      const idxIndex = activatedChoices.indexOf(idx);
+                      const deletedAcitvatedChocies = [...activatedChoices];
+                      deletedAcitvatedChocies.splice(idxIndex, 1);
+                      setActivatedChoices(deletedAcitvatedChocies);
+                    }
+                  }}
+                >
+                  <option>단문형</option>
+                  <option>장문형</option>
+                  <option>객관식</option>
+                </select>
+              </div>
+
+              {/*placeholder 입력기: 단문형과 장문형에서만 떠야*/}
+              <div className="flex flex-row justify-center w-full">
+                <input
+                  id="placeholding"
+                  className="input-b-1 mb-2 justify-self-center"
+                  placeholder="이용자가 입력값 내용을 어떻게 작성하면 좋을지 설명해주세요."
+                  style={{
+                    display: activatedChoices.includes(idx) ? "none" : "block",
+                  }}
+                ></input>
+              </div>
+              {/*객관식 선지 입력기: 객관식에서만 떠야*/}
+              {el["options"].map((el2, idx2) => (
+                <div
+                  className="flex flex-row justify-around mb-2"
+                  style={{
+                    display: activatedChoices.includes(idx) ? "flex" : "none",
+                  }}
+                >
+                  <input
+                    key={"options" + idx}
+                    id={"options" + idx}
+                    className="input-e mr-8"
+                    onChange={(e) => {
+                      const newOptions = [...form[idx].options];
+                      const newOptionValue = e.target.value;
+                      newOptions.splice(idx2, 1, newOptionValue);
+                      const newOptionForm = {
+                        ...form[idx],
+                        options: newOptions,
+                      };
+                      const newForm = [...form];
+                      newForm.splice(idx, 1, newOptionForm);
+                      setForm(newForm);
+                    }}
+                    value={el2}
+                    placeholder="객관식 선택지"
+                  ></input>
+                  <button
+                    className="button-plus-blue mr-2"
+                    onClick={() => {
+                      const newOptions = [...form[idx].options, ""];
+                      const newOptionForm = {
+                        ...form[idx],
+                        options: newOptions,
+                      };
+                      const newForm = [...form];
+                      newForm.splice(idx, 1, newOptionForm);
+                      setForm(newForm);
+                    }}
+                  >
+                    +
+                  </button>
+                  <button
+                    type="button"
+                    className="button-plus-red mr-2"
+                    onClick={() => {
+                      const deletedOption = [...form[idx].options];
+                      const deletedOptionForm = {
+                        ...form[idx],
+                        options: deletedOption,
+                      };
+                      deletedOption.splice(idx2, 1);
+                      const newForm = [...form];
+                      newForm.splice(idx, 1, deletedOptionForm);
+                      setForm(newForm);
+                      console.log(newForm);
+                    }}
+                  >
+                    -
+                  </button>
+                </div>
+              ))}
+
+              {/*삭제버튼(누르면 입력값 한 세트가 삭제됨)*/}
+              <button
+                type="button"
+                className="button-e items-center mb-8"
+                onClick={() => {
+                  const deletedForm = [...form];
+                  deletedForm.splice(idx, 1);
+                  setForm(deletedForm);
+                }}
+              >
+                <img src={trashBin} className="w-4 h-4"></img>
+              </button>
+            </>
+          ))}{" "}
         </div>
         <div className="flex flex-col items-center">
-          {/*입력값 input창을 추가해주는 버튼*/}
+          {/*입력값 추가(아래 버튼을 누르면 폼이 하나씩 추가됨)*/}
           <button
             type="submit"
-            className="button-plus"
+            className="button-a mt-4"
             onClick={() => {
-              setForm([...form, <input className="input-b my-4"/>]);
-            }}>
-            +
+              setForm([
+                ...form,
+                {
+                  label: "",
+                  type: "단문형",
+                  placeholding: "",
+                  options: [""],
+                },
+              ]);
+            }}
+          >
+            입력값 추가
           </button>
         </div>
       </div>
@@ -83,7 +219,7 @@ const PromptMakePage = () => {
       {/*오른쪽 절반*/}
       <div className="bg-white text-black w-1/2 h-full p-10 pr-20">
         <h1 className="text-5xl font-bold mx-6 mb-8">프롬프트</h1>
-        <div className="rounded-3xl bg-gray-200  mx-6 h-3/5 w-full flex flex-col">
+        <div className="rounded-3xl bg-gray-200  mx-6 h-1/2 w-full flex flex-col">
           <textarea
             className="w-auto h-5/6 bg-gray-200 overflow-y-auto focus:border-transparent m-4 p-6 resize-none outline-none"
             placeholder=" ChatGPT에게 전달될 프롬프트를 작성해주세요.
@@ -91,16 +227,45 @@ const PromptMakePage = () => {
       &#13;&#10; [예시]
       &#13;&#10; 미팅 회의록을 요약해줘. 회의 주제는 #회의주제 이고,
       &#13;&#10; 요약할 때 말투는 #말투 로 해줘."
-
-      value = {content} onChange={(e) => {setContent(e.target.value)}}></textarea>
-      <button className="button-a self-end m-8">미리보기</button>
-    </div>
-    {/*삭제&게시 버튼*/}
-    <div className="mt-12 flex justify-center">
-    <button className="button-e mr-16" type="reset">삭제하기</button>
-    <button className="button-d ml-16" /*onClick=서버로 프롬프트 덩어리를 보내는 함수*/>게시하기</button>
-</div>
-    </div>
+            value={content}
+            onChange={(e) => {
+              setContent(e.target.value);
+            }}
+          ></textarea>
+          <button className="button-a self-end m-8">미리보기</button>
+        </div>
+        {/*카테고리*/}
+        <h1 className="text-2xl font-bold mx-6 mt-8">카테고리</h1>
+        <select
+          onChange={(e) => {
+            setCategory(e.target.value);
+          }}
+          className="text-black w-32 h-12 mt-4 mx-6 border-gpt-green border-2 rounded-xl font-medium text-center shadow"
+        >
+          <option>과제</option>
+          <option>발표</option>
+          <option>여가</option>
+          <option>대외활동</option>
+          <option>컴퓨팅</option>
+          <option>진로</option>
+          <option>커뮤니케이션(연락)</option>
+          <option>일정</option>
+        </select>
+        {/*삭제&게시 버튼*/}
+        <div className="mt-12 flex justify-center">
+          <button className="button-e mr-16" type="reset">
+            삭제하기
+          </button>
+          <button
+            className="button-d ml-16"
+            onClick={() => console.log(prompt)}
+            //axios 통해 서버로 프롬프트 덩어리를 보내는 함수가 있어야!
+            //지금은 잘 들어왔는지 확인하기 위해 콘솔로그 찍는 기능 넣어놨어유
+          >
+            게시하기
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
--- src/routes/PromptResultPage.jsx
+++ src/routes/PromptResultPage.jsx
@@ -33,7 +33,7 @@ const PromptResultPage = () => {
     <form className="w-4/5 h-3/4 flex flex-col items-center bg-gray-200 text-black p-11 rounded-3xl ">
       {/*사용자 측 프롬프트 질문*/}
   <div className="flex justify-between mx-20">
-  <div className="bubble-a font-medium overflow-x-hidden overflow-y-auto">각 지방의 내일 날씨입니다. 내일은 맑은 뒤 구름이 많이 끼겠고, 제주도와 울릉도 독도에는 한두 차례 눈이 오겠습니다. 아침 최저기온은 중서부 지방이 영하 23도에서 영하 14도, 영동과 남부지방은 영하 13도에서 영하 8도로 몹시 추운 날씨가 내일 아침까지도 계속되겠습니다.
+  <div className="bubble-a font-medium overflow-x-hidden items-center overflow-y-auto ">각 지방의 내일 날씨입니다. 내일은 맑은 뒤 구름이 많이 끼겠고, 제주도와 울릉도 독도에는 한두 차례 눈이 오겠습니다. 아침 최저기온은 중서부 지방이 영하 23도에서 영하 14도, 영동과 남부지방은 영하 13도에서 영하 8도로 몹시 추운 날씨가 내일 아침까지도 계속되겠습니다.
 그렇지만 낮 최고 기온은 영하 6도에서 영상 1도의 분포로 오늘보다 3-4도 가량 올라갈 전망입니다.
 </div>
   <div className="bubble-a:left"/>
@@ -43,13 +43,13 @@ const PromptResultPage = () => {
 {/*GPT 답변*/} 
 <div className="flex justify-between mx-20">
 <img id="gpt-logo" src={gpt_logo} alt="gpt_logo" className="flex flex-col items-center mr-10 w-28 h-28"/>
-<div className="bubble-b font-medium overscroll-y-auto">각 지방의 내일 날씨입니다. 내일은 맑은 뒤 구름이 많이 끼겠고, 제주도와 울릉도 독도에는 한두 차례 눈이 오겠습니다. 아침 최저기온은 중서부 지방이 영하 23도에서 영하 14도, 영동과 남부지방은 영하 13도에서 영하 8도로 몹시 추운 날씨가 내일 아침까지도 계속되겠습니다.
+<div className="bubble-b font-medium overscroll-y-auto line-clamp-4">각 지방의 내일 날씨입니다. 내일은 맑은 뒤 구름이 많이 끼겠고, 제주도와 울릉도 독도에는 한두 차례 눈이 오겠습니다. 아침 최저기온은 중서부 지방이 영하 23도에서 영하 14도, 영동과 남부지방은 영하 13도에서 영하 8도로 몹시 추운 날씨가 내일 아침까지도 계속되겠습니다.
 그렇지만 낮 최고 기온은 영하 6도에서 영상 1도의 분포로 오늘보다 3-4도 가량 올라갈 전망입니다.
 날씨를 전해 드렸습니다.</div>
   <div className="bubble-b:right"/>
   </div>
   {/*버튼*/}
-  <button type="submit" className=" bg-gpt-green text-white font-bold hover:text-black rounded-3xl text-lg py-3.5 px-20 shadow-xl">
+  <button type="button" onclick="history.back()" className=" bg-gpt-green text-white font-bold hover:text-black rounded-3xl text-lg py-3.5 px-20 shadow-xl">
   나가기
   </button>
   </form>
